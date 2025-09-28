acd26428aa01e1f19a9aa816c4530cfe4f96b285
JauniKappauni <social.jauni@gmail.com>
2025-09-28 15:51:18 +0200

    Added gm command

--- src/main/java/de/jauni/axCore/AxCore.java
+++ src/main/java/de/jauni/axCore/AxCore.java
@@ -10,6 +10,7 @@ public final class AxCore extends JavaPlugin {
         getCommand("heal").setExecutor(new de.jauni.axCore.commands.HealCommand());
         getCommand("feed").setExecutor(new de.jauni.axCore.commands.FeedCommand());
         getCommand("fly").setExecutor(new de.jauni.axCore.commands.FlyCommand());
+        getCommand("gm").setExecutor(new de.jauni.axCore.commands.GameModeCommand());
     }
 
     @Override
---
+++ src/main/java/de/jauni/axCore/commands/GameModeCommand.java
@@ -0,0 +1,33 @@
+package de.jauni.axCore.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GameModeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        Player player = (Player) sender;
+        if(args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Bitte gib einen Spielmodus an.\nMögliche Optionen: survival, creative, adventure, spectator");
+            return false;
+        }
+        String mode = args[0].toLowerCase();
+        switch (mode) {
+            case "survival", "0" -> player.setGameMode(GameMode.SURVIVAL);
+            case "creative", "1" -> player.setGameMode(GameMode.CREATIVE);
+            case "adventure", "2" -> player.setGameMode(GameMode.ADVENTURE);
+            case "spectator", "3" -> player.setGameMode(GameMode.SPECTATOR);
+            default -> {
+                player.sendMessage(ChatColor.RED + "Ungültiger Spielmodus.\nMögliche Optionen: survival, creative, adventure, spectator");
+                return false;
+            }
+        }
+        player.sendMessage(ChatColor.GREEN + "Spielmodus geändert zu " + mode);
+        return  true;
+    }
+}
