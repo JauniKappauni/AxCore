6678c8e7ef54971e428a971ced9c987e596f9c56
JauniKappauni <social.jauni@gmail.com>
2025-09-28 14:13:18 +0200

    Add initial project

---
+++ src/main/java/de/jauni/axCore/AxCore.java
@@ -0,0 +1,17 @@
+package de.jauni.axCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
