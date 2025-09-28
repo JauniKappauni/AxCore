28fa72e62a648058d9362895bef36154e6c0d810
JauniKappauni <social.jauni@gmail.com>
2025-09-28 16:39:44 +0200

    Add argument check and feedback messages

--- src/main/java/de/jauni/axCore/commands/TeleportCommand.java
+++ src/main/java/de/jauni/axCore/commands/TeleportCommand.java
@@ -1,5 +1,6 @@
 package de.jauni.axCore.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,9 +11,15 @@ public class TeleportCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Bitte gib einen Spielernamen an.");
+            return false;
+        }
         Player player = (Player) sender;
         Player player2 = Bukkit.getPlayer(args[0]);
         player.teleport(player2.getLocation());
+        player.sendMessage(ChatColor.GREEN + "Du hast dich zu " + player2.getName() + " teleportiert!");
+        player2.sendMessage(ChatColor.GREEN + "" + player.getName() + " hat sich zu dir teleportiert!");
         return true;
     }
 
--- src/main/java/de/jauni/axCore/commands/TeleportHereCommand.java
+++ src/main/java/de/jauni/axCore/commands/TeleportHereCommand.java
@@ -1,5 +1,6 @@
 package de.jauni.axCore.commands;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,9 +11,15 @@ public class TeleportHereCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Bitte gib einen Spielernamen an.");
+            return false;
+        }
         Player player = (Player) sender;
         Player player2 = Bukkit.getPlayer(args[0]);
         player2.teleport(player.getLocation());
+        player2.sendMessage(ChatColor.GREEN + "Du hast dich zu " + player.getName() + " teleportiert!");
+        player.sendMessage(ChatColor.GREEN + "" + player2.getName() + " hat sich zu dir teleportiert!");
         return true;
     }
 }
